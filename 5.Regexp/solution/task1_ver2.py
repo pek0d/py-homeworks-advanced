ecf74498633b6c656e817c8d3be0ec93f8827cce
pekod <onathema@gmail.com>
2024-01-17 08:48:12 +0300

    format phone number function added

--- 5.Regexp/solution/task1_ver2.py
+++ 5.Regexp/solution/task1_ver2.py
@@ -1,84 +1,92 @@
 import csv
 import re
-from pprint import pprint
+
+with open("../phonebook_raw.csv", encoding="utf-8") as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
 
 
-# читаем адресную книгу в формате CSV в список contacts_list
-def read_csv():
-    with open("../phonebook_raw.csv") as f:
-        rows = csv.reader(f, delimiter=",")
-        contacts_list = list(rows)
-    return contacts_list
-
-
-# pprint(contacts_list)
-
-
-def process_contacts(contact) -> None:
-    """Функция для обработки контакта"""
-    name = contact[0].split()
-    if len(name) > 0:
-        contact[0] = name[0]  # Фамилия
-    if len(name) > 1:
-        contact[1] = name[1]  # Имя
-    if len(name) > 2:
-        contact[2] = " ".join(name[2:])  # Отчество
-
-
-def split_name(full_name) -> list:
+def split_name(names_list) -> list:
     """Функция для разделения полного имени на отчество, имя и фамилию"""
-    return full_name.split()
+
+    full_name = ["", "", ""]
+    start = 0
+    for name in names_list:
+        if name:
+            name = name.split()
+            stop = len(name)
+            full_name[start : start + stop] = name
+            start += stop
+
+    return full_name
+
+
+def remove_duplicates(contacts_list) -> list:
+    """Удаление дублирующихся контактов"""
+
+    result_dict = {}
+    for row in contacts_list:
+        key = row[0], row[1]
+        data_dict = {
+            "surname": row[2],
+            "organization": row[3],
+            "position": row[4],
+            "phone": row[5],
+            "email": row[6],
+        }
+        if result_dict.get(key):
+            for value in result_dict.get(key):
+                if row[2] in value.get("surname"):
+                    value.update(
+                        {key: data_dict.get(key) for key in value if not value.get(key)}
+                    )
+                else:
+                    result_dict.get(key).append(data_dict)
+        else:
+            result_dict[key] = [data_dict]
+    return result_dict
 
 
 def extract_names(contacts_list) -> list:
     """Функция для извлечения имен из списка контактов"""
+
     for row in contacts_list:
-        full_name = " ".join(
-            row[:3]
-        )  # Объединяем фамилию, имя и отчество в одну строку
-        processed_name = split_name(
-            full_name
-        )  # Разделяем полное имя на отдельные части: фамилию, имя и отчество
-        row[
-            :3
-        ] = processed_name  # Обновляем список контакта с разделенными частями имени
-        process_contacts(
-            row
-        )  # Обрабатываем контакт, соответственно вызывая функцию process_contacts
+        row[:3] = split_name(row[:3])
+        row[5] = format_phone_number(row[5])
     return contacts_list
 
 
-def remove_duplicates(contacts_list) -> list:
-    """Удаление дублирующихся контактов и форматирование телефонных номеров"""
-    seen_names = set()
-    cleaned_contacts = []
+def format_phone_number(phone):
+    """Функция для форматирования телефонного номера"""
 
-    for contact in contacts_list:
-        name = (
-            contact[0],
-            contact[1],
-        )  # Создаем кортеж (имя, фамилия) для использования в качестве ключа
-        if name not in seen_names:
-            seen_names.add(
-                name
-            )  # Добавляем комбинацию (имя, фамилия) в множество уже увиденных имен
-
-            cleaned_contacts.append(
-                contact
-            )  # Добавляем контакт в список обновленных контактов
-
-    return cleaned_contacts
+    pattern = r"\+?([7|8])\s?\(?(\d{3})\)?[\s-]?(\d{3,})\-?(\d{2,})\-?(\d{2,})\s?\(?(\w{3})?\.?\s?(\d{4})?\)?"
+    formatted_number = re.sub(pattern, r"+7(\2) \3-\4-\5 \6 \7", phone)
+    return formatted_number
 
 
-def write_cleaned_contacts(cleaned_contacts) -> None:
+def write_contacts(result_dict, header) -> None:
     """Функция для записи обновленного списка контактов без дубликатов в файл"""
+
     with open("phonebook.csv", "w", encoding="utf-8", newline="") as f:
-        datawriter = csv.writer(f, delimiter=",")
-        datawriter.writerows(cleaned_contacts)
+        datawriter = csv.DictWriter(f, fieldnames=header, delimiter=",")
+        datawriter.writeheader()
+        for key, value_list in result_dict.items():
+            for value in value_list:
+                datawriter.writerow(
+                    {
+                        "lastname": key[0],
+                        "firstname": key[1],
+                        "surname": value.get("surname"),
+                        "organization": value.get("organization"),
+                        "position": value.get("position"),
+                        "phone": value.get("phone"),
+                        "email": value.get("email"),
+                    }
+                )
 
 
 if __name__ == "__main__":
-    contacts_list = read_csv()
-    extract_names(contacts_list)
-    cleaned_contacts = remove_duplicates(contacts_list)
-    write_cleaned_contacts(cleaned_contacts)
+    extract_names(contacts_list[1:])
+    remove_duplicates_dict = remove_duplicates(contacts_list[1:])
+    header = contacts_list[0]
+    write_contacts(remove_duplicates_dict, header)
