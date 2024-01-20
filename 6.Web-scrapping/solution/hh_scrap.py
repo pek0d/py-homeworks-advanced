0f10a863722d6cff0603d9d47c7eb0eee86da75b
pekod <onathema@gmail.com>
2024-01-20 15:06:58 +0300

    UPD for cycle for response

--- 6.Web-scrapping/solution/hh_scrap.py
+++ 6.Web-scrapping/solution/hh_scrap.py
@@ -4,51 +4,50 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-# URL, который нужно запросить
-url = "https://spb.hh.ru/search/vacancy?text=python+django+flask&area=1&area=2"
-
-# Заголовки (Headers)
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
     "Chrome/91.0.4472.124 Safari/537.36"
 }
 
-# Отправка GET-запроса с использованием заголовков
-response = requests.get(url, headers=headers)
+for page in range(10):
+    url = f"https://spb.hh.ru/search/vacancy?text=python+django+flask&area=1&area=2&page={page}"
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.content, "html.parser")
+    response = requests.get(url, headers=headers)
 
-    # Находим все вакансии
-    vacancies = soup.find_all("div", class_="vacancy-serp-item__layout")
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
 
-    results = []
-    for vacancy in vacancies:
-        title = vacancy.find("span", class_="serp-item__title").text
-        company = vacancy.find("a", {"data-qa": "vacancy-serp__vacancy-employer"}).text
-        address = vacancy.find("div", {"data-qa": "vacancy-serp__vacancy-address"}).text
-        city = address.split(",")[0]
+        vacancies = soup.find_all("div", class_="vacancy-serp-item__layout")
 
-        salary_element = (
-            "span",
-            {"data-qa": "vacancy-serp__vacancy-compensation"},
-        )
-        salary = salary_element.text if salary_element else print("не указано")
+        results = []
+        for vacancy in vacancies:
+            company = vacancy.find(
+                "a", {"data-qa": "vacancy-serp__vacancy-employer"}
+            ).text
+            address = vacancy.find(
+                "div", {"data-qa": "vacancy-serp__vacancy-address"}
+            ).text
+            city = address.split(",")[0]
 
-        link = vacancy.find("a", class_="bloko-link")["href"]
-
-        if re.search(r"\bUSD\b", str(salary)) or re.search(
-            r"dollar", str(salary), re.IGNORECASE
-        ):
-            results.append(
-                {
-                    "title": title,
-                    "company": company,
-                    "city": city,
-                    "salary": str(salary.text.strip()) if salary else None,
-                    "link": link,
-                }
+            salary_element = vacancy.find(
+                "span",
+                {"data-qa": "vacancy-serp__vacancy-compensation"},
             )
+            salary = salary_element.text if salary_element else "Не указана"
 
-    with open("vacancies_filtered.json", "w", encoding="utf-8") as f:
-        json.dump(results, f, ensure_ascii=False, indent=4)
+            link = vacancy.find("a", class_="bloko-link")["href"]
+
+            if re.search(r"\bUSD\b", str(salary)) or re.search(
+                r"dollar", str(salary), re.IGNORECASE
+            ):
+                results.append(
+                    {
+                        "company": company,
+                        "city": city,
+                        "salary": salary,
+                        "link": link,
+                    }
+                )
+
+        with open("vacancies_filtered.json", "w", encoding="utf-8") as f:
+            json.dump(results, f, ensure_ascii=False, indent=4)
