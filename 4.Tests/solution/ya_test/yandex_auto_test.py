b78bc9a8b812b6599460cba3ee37906474a462c6
pekod <onathema@gmail.com>
2024-02-03 14:17:22 +0300

    test fix

--- 4.Tests/solution/ya_test/yandex_auto_test.py
+++ 4.Tests/solution/ya_test/yandex_auto_test.py
@@ -16,33 +16,16 @@ class TestYandexDiskAPI(unittest.TestCase):
         folder_name = "TestFolder"
         create_folder_url = f"{self.folder_url}?path={folder_name}"
 
-        # Make a POST request to create a folder
         response = requests.put(create_folder_url, headers=self.headers)
 
-        # Assert that the response code is 201 (Created)
         self.assertEqual(response.status_code, 201)
-
-        # Optional: Assert that the folder appears in the list of files
-        # You can make a separate GET request to the folder URL and verify its presence
-
-    def test_create_folder_failure_invalid_path(self):
-        invalid_folder_name = "InvalidPath**"
-        create_folder_url = f"{self.folder_url}?path=/app/{invalid_folder_name}"
-
-        # Make a POST request with an invalid folder name
-        response = requests.put(create_folder_url, headers=self.headers)
-
-        # Assert that the response code is not 201 (Created)
-        self.assertNotEqual(response.status_code, 201)
 
     def test_create_folder_failure_no_permission(self):
         folder_name = "ProtectedFolder"
-        create_folder_url = f"{self.folder_url}?path=/app/{folder_name}"
+        create_folder_url = f"{self.folder_url}?path={folder_name}"
 
-        # Make a POST request to create a folder in a location without permission
         response = requests.put(create_folder_url, headers=self.headers)
 
-        # Assert that the response code is not 201 (Created)
         self.assertNotEqual(response.status_code, 201)
 
 
